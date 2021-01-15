c43129f57bf83013d9a2461f68d4a824e945c519
Stella Cai <stella.sijiacai@gmail.com>
2021-01-15 14:31:46 -0500

    changed api to https

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
 export const APIKEY = "c31a6f15";
 // export const URL = "http://www.omdbapi.com/?apikey=["+ APIKEY + "]&"
-export const URL = "http://www.omdbapi.com/"
+export const URL = "https://www.omdbapi.com/"
